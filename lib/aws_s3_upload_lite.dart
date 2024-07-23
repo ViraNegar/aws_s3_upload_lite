5ee55f2c606971b0705a586fc1b293217e59e681
MohammadGhamary <mohammadghamary76@outlook.com>
2024-07-23 09:18:23 +0330

    use Dio package to send requests

--- lib/aws_s3_upload_lite.dart
+++ lib/aws_s3_upload_lite.dart
@@ -5,19 +5,18 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'package:path/path.dart' as path;
 import 'package:recase/recase.dart';
 import './src/policy.dart';
-import 'src/multipart_request_with_progress.dart';
-import 'src/streamed_request_with_progress.dart';
 import 'src/upload_progress.dart';
 import 'enum/acl.dart';
 import 'src/utils.dart';
+
 /// Convenience class for uploading files to AWS S3
 class AwsS3 {
   /// Upload a file, returning the status code 200/204 on success.
-  static Future<Stream<UploadProgress>> uploadFile({
+  static Stream<UploadProgress> uploadFile({
     /// Storage endpoint
     required String domain,
 
@@ -57,68 +56,88 @@ class AwsS3 {
 
     /// Additional metadata to be attached to the upload
     Map<String, String>? metadata,
-  }) async {
-    var httpStr = 'http';
-    if (useSSL) {
-      httpStr += 's';
+
+    required CancelToken cancelToken
+  }) async* {
+    final StreamController<UploadProgress> streamController =
+    StreamController<UploadProgress>();
+    try {
+      var httpStr = 'http';
+      if (useSSL) {
+        httpStr += 's';
+      }
+      final endpoint = '$httpStr://$domain/$bucket';
+
+      String? uploadKey;
+
+      if (key != null) {
+        uploadKey = key;
+      } else if (destDir.isNotEmpty) {
+        uploadKey = '$destDir/$filename';
+      } else {
+        uploadKey = '$filename';
+      }
+
+      final length = file.lengthSync();
+
+      final dio = Dio();
+
+      // Convert metadata to AWS-compliant params before generating the policy.
+      final metadataParams = _convertMetadataToParams(metadata);
+
+      // Generate pre-signed policy.
+      final policy = Policy.fromS3PresignedPost(
+        uploadKey,
+        bucket,
+        accessKey,
+        15,
+        length,
+        acl,
+        region: region,
+        metadata: metadataParams,
+      );
+
+      final signingKey =
+      SigV4.calculateSigningKey(secretKey, policy.datetime, region, 's3');
+      final signature = SigV4.calculateSignature(signingKey, policy.encode());
+
+      FormData formData = FormData.fromMap({
+        'key': policy.key,
+        'acl': aclToString(acl),
+        'X-Amz-Credential': policy.credential,
+        'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
+        'X-Amz-Date': policy.datetime,
+        'Policy': policy.encode(),
+        'X-Amz-Signature': signature,
+        'Content-Type': contentType,
+        'file': await MultipartFile.fromFile(
+            file.path, filename: path.basename(file.path)),
+      });
+
+      int startTime = DateTime
+          .now()
+          .millisecondsSinceEpoch;
+      int lastSent = 0;
+
+      dio.post(
+        endpoint,
+        data: formData,
+        cancelToken: cancelToken,
+        onSendProgress: (sent, total) {
+          int now = DateTime
+              .now()
+              .millisecondsSinceEpoch;
+          int elapsed = now - startTime;
+          lastSent = sent;
+          streamController.add(UploadProgress(sent, total, elapsed));
+        },
+      );
+    } catch (e) {
+      streamController.addError(e);
+      streamController.close();
     }
-    final endpoint = '$httpStr://$domain/$bucket';
 
-    String? uploadKey;
-
-    if (key != null) {
-      uploadKey = key;
-    } else if (destDir.isNotEmpty) {
-      uploadKey = '$destDir/$filename';
-    } else {
-      uploadKey = '$filename';
-    }
-
-    final stream = http.ByteStream(Stream.castFrom(file.openRead()));
-    final length = file.lengthSync();
-
-    final uri = Uri.parse(endpoint);
-    final req = MultipartRequestWithProgress("POST", uri);
-    final multipartFile = http.MultipartFile('file', stream, length,
-        filename: path.basename(file.path));
-
-    // Convert metadata to AWS-compliant params before generating the policy.
-    final metadataParams = _convertMetadataToParams(metadata);
-
-    // Generate pre-signed policy.
-    final policy = Policy.fromS3PresignedPost(
-      uploadKey,
-      bucket,
-      accessKey,
-      15,
-      length,
-      acl,
-      region: region,
-      metadata: metadataParams,
-    );
-
-    final signingKey =
-    SigV4.calculateSigningKey(secretKey, policy.datetime, region, 's3');
-    final signature = SigV4.calculateSignature(signingKey, policy.encode());
-
-    req.files.add(multipartFile);
-    req.fields['key'] = policy.key;
-    req.fields['acl'] = aclToString(acl);
-    req.fields['X-Amz-Credential'] = policy.credential;
-    req.fields['X-Amz-Algorithm'] = 'AWS4-HMAC-SHA256';
-    req.fields['X-Amz-Date'] = policy.datetime;
-    req.fields['Policy'] = policy.encode();
-    req.fields['X-Amz-Signature'] = signature;
-    req.fields['Content-Type'] = contentType;
-
-    // If metadata isn't null, add metadata params to the request.
-    if (metadata != null) {
-      req.fields.addAll(metadataParams);
-    }
-
-    final client = StreamedRequestWithProgress(http.Client());
-    client.send(req);
-    return req.progressStream;
+    yield* streamController.stream;
   }
 
   /// A method to transform the map keys into the format compliant with AWS.
@@ -139,112 +158,5 @@ class AwsS3 {
   /// A method to convert File To Cast
   static convertFileToCast(Uint8List data) {
     return List<int>.from(data);
-  }
-
-  /// Upload a Uint8List, returning the status code 200/204 on success.
-  static Future<Stream<UploadProgress>> uploadUint8List({
-
-    /// Storage endpoint
-    required String domain,
-
-    /// AWS access key
-    required String accessKey,
-
-    /// AWS secret key
-    required String secretKey,
-
-    /// The name of the S3 storage bucket to upload  to
-    required String bucket,
-
-    /// The file to upload
-    required Uint8List file,
-
-    /// The AWS region. Must be formatted correctly, e.g. us-west-1
-    required String region,
-
-    /// The path to upload the file to (e.g. "uploads/public"). Defaults to the root "directory"
-    required String destDir,
-
-    /// The filename to upload as.
-    required String filename,
-
-    /// The key to save this file as. Will override destDir and filename if set.
-    String? key,
-
-    /// Access control list enables you to manage access to bucket and objects
-    /// For more information visit [https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl-overview.html]
-    ACL acl = ACL.public_read,
-
-    /// The content-type of file to upload. defaults to binary/octet-stream.
-    String contentType = 'binary/octet-stream',
-
-    /// If set to true, https is used instead of http. Default is true.
-    bool useSSL = true,
-
-    /// Additional metadata to be attached to the upload
-    Map<String, String>? metadata,
-  }) async {
-    var httpStr = 'http';
-    if (useSSL) {
-      httpStr += 's';
-    }
-
-    final endpoint = '$httpStr://$domain/$bucket';
-
-    String? uploadKey;
-
-    if (key != null) {
-      uploadKey = key;
-    } else if (destDir.isNotEmpty) {
-      uploadKey = '$destDir/$filename';
-    } else {
-      uploadKey = '$filename';
-    }
-
-    final stream = http.ByteStream(convertFileToCast(file));
-    final length = file.lengthInBytes;
-
-    final uri = Uri.parse(endpoint);
-    final req = MultipartRequestWithProgress("POST", uri);
-    final multipartFile =
-    http.MultipartFile('file', stream, length, filename: filename);
-
-    // Convert metadata to AWS-compliant params before generating the policy.
-    final metadataParams = _convertMetadataToParams(metadata);
-
-    // Generate pre-signed policy.
-    final policy = Policy.fromS3PresignedPost(
-      uploadKey,
-      bucket,
-      accessKey,
-      15,
-      length,
-      acl,
-      region: region,
-      metadata: metadataParams,
-    );
-
-    final signingKey =
-    SigV4.calculateSigningKey(secretKey, policy.datetime, region, 's3');
-    final signature = SigV4.calculateSignature(signingKey, policy.encode());
-
-    req.files.add(multipartFile);
-    req.fields['key'] = policy.key;
-    req.fields['acl'] = aclToString(acl);
-    req.fields['X-Amz-Credential'] = policy.credential;
-    req.fields['X-Amz-Algorithm'] = 'AWS4-HMAC-SHA256';
-    req.fields['X-Amz-Date'] = policy.datetime;
-    req.fields['Policy'] = policy.encode();
-    req.fields['X-Amz-Signature'] = signature;
-    req.fields['Content-Type'] = contentType;
-
-    // If metadata isn't null, add metadata params to the request.
-    if (metadata != null) {
-      req.fields.addAll(metadataParams);
-    }
-
-    final client = StreamedRequestWithProgress(http.Client());
-    client.send(req);
-    return req.progressStream;
   }
 }
